52f4ef88c82d81e2547f0fc638d683c3ea9cd892
Your Name <you@example.com>
2024-08-29 11:59:20 +0500

    minor changes

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -2,7 +2,7 @@ import 'package:cusit/screens/auth/splash_screen.dart';
 import 'package:cusit/screens/auth/staff_login_screen.dart';
 import 'package:cusit/screens/auth/upgradeappscreen.dart';
 import 'package:cusit/screens/admission/admission_screen.dart';
-import 'package:cusit/screens/chat/user/chat_screen.dart';
+import 'package:cusit/screens/chat/staff/staffdashboard_screen.dart';
 import 'package:cusit/screens/dashboard/dashboard_screen.dart';
 import 'package:cusit/screens/program/programs_screen.dart';
 import 'package:cusit/screens/prospectus/prospectus_screen.dart';
@@ -12,6 +12,7 @@ class AppRoutes {
   static Map<String, Widget Function(BuildContext)> routes = {
     SplashScreen.id: (context) => const SplashScreen(),
     LoginScreen.id: (context) => const LoginScreen(),
+    StaffDashBoardScreen.id: (context) => const StaffDashBoardScreen(),
     UpgradeScreen.id: (context) => const UpgradeScreen(),
     DashboardScreen.id: (context) => const DashboardScreen(),
     AdmissionScreen.id: (context) => const AdmissionScreen(),
